f46b5a693703cfb549eae15f82efde3db7670480
sahilraj76ind <sahilraj.cse28@jecrc.ac.in>
2026-06-30 00:34:01 +0530

    Time: 3 ms (63.65%) | Memory: 44 MB (19.08%) - LeetSync

---
+++ 1666-make-the-string-great/make-the-string-great.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String makeGood(String s) {
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+
+            if (!stack.isEmpty() &&
+                Math.abs(ch - stack.peek()) == 32) {
+
+                stack.pop();
+            } else {
+                stack.push(ch);
+            }
+        }
+
+        StringBuilder ans = new StringBuilder();
+
+        while (!stack.isEmpty()) {
+            ans.append(stack.pop());
+        }
+
+        return ans.reverse().toString();
+    }
+}
